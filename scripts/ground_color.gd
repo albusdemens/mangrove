fbb0322b42d3934cc9938c2d23960e44e979c4c9
Alberto Cereser <mannaro85@gmail.com>
2025-04-26 00:36:21 +0900

    <add>:: world environment (to be refined)

--- scripts/ground_color.gd
+++ scripts/ground_color.gd
@@ -2,7 +2,7 @@ extends Sprite3D
 
 func _ready():
 	# Set the color to red
-	modulate = Color("#fe6d4e")
+	modulate = Color("#c74300")
 
 	# Set the color to blue with 50% transparency
 	#modulate = Color(0.0, 0.0, 1.0, 0.5)
